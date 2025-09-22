77e24eafbb5aad79852418a19ef3782d219f142c
kkkwwww-ui <129519318+kkkwwww-ui@users.noreply.github.com>
2025-09-22 17:48:29 -0400

    implemented Camera class, basic drivetrain also added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class Camera {
+
+    private OpMode opMode;
+    private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal visionPortal;
+
+    public Camera(OpMode linearOpMode) {
+        //initialize opmode variable for current opmode
+        this.opMode = linearOpMode;
+
+        //create april tag processor
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+
+                // The following default settings are available to un-comment and edit as needed.
+                //.setDrawAxes(false)
+                //.setDrawCubeProjection(false)
+                //.setDrawTagOutline(true)
+                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+
+                // == CAMERA CALIBRATION ==
+                // C310 has preset callibrated values in the sdk
+
+                .build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTagProcessor.setDecimation(2);
+
+        //use builder pattern to configure vision portal settings
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(opMode.hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.addProcessor(aprilTagProcessor);
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        //create the vision portal
+        visionPortal = builder.build();
+
+
+    }
+
+    //TODO: make methods for detecting a specific apriltag, detecting the current pattern, and getting the pose
+}
