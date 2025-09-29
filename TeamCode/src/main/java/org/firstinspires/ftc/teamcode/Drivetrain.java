f9565225c03ac32c77456f8e75dca48565b4a89c
kkkwwww-ui <129519318+kkkwwww-ui@users.noreply.github.com>
2025-09-29 16:45:20 -0400

    implemented untested field centric drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Drivetrain {
     //declare motors
@@ -10,7 +14,10 @@ public class Drivetrain {
     public DcMotor backLeft;
     public DcMotor frontRight;
     public DcMotor backRight;
+    public IMU imu;
+    public double botHeading;
     private OpMode opMode;
+
 
     //constructor which acts as an initialization function for whenever an object of the class is created
     public Drivetrain(OpMode linearopmode)
@@ -20,9 +27,25 @@ public class Drivetrain {
         backLeft = opMode.hardwareMap.get(DcMotor.class, "backleft");
         frontRight = opMode.hardwareMap.get(DcMotor.class, "frontright");
         backRight = opMode.hardwareMap.get(DcMotor.class, "backright");
+        imu = opMode.hardwareMap.get(IMU.class, "imu");
+
+        // adjust the orientation parameters
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // actually set the parameters
+        imu.initialize(parameters);
 
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void resetIMU() {
+        imu.resetYaw();
+    }
+
+    public void updateIMU() {
+        botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
     }
 
     //main mecanum drive function
@@ -30,8 +53,12 @@ public class Drivetrain {
     {
         //get gamepad inputs
         double y = -gamepad.left_stick_y;
-        double x = gamepad.left_stick_x * 1.1;
+        double x = gamepad.left_stick_x;
         double rx = gamepad.right_stick_x;
+
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+        rotX = rotX * 1.1;
 
         //speed control with triggers
         double maxSpeed = 0.5;
@@ -39,11 +66,11 @@ public class Drivetrain {
         maxSpeedMultiplier = maxSpeed + ((-gamepad.right_trigger * (maxSpeed * 0.5)) + (gamepad.left_trigger * maxSpeed));
 
         //solve for power
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
 
         //multiply calculated power with the speed control to obtain final power for the motors
         frontLeft.setPower(frontLeftPower * maxSpeedMultiplier);
