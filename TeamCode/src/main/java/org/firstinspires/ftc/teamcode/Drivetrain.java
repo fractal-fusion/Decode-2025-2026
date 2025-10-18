b7759075738bab95d55b2d37607ef6d416933692
kkkwwww-ui <129519318+kkkwwww-ui@users.noreply.github.com>
2025-10-18 14:45:48 -0400

    FIXUP! just added some todo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -25,6 +25,8 @@ public class Drivetrain {
     public static double AUTO_ALIGN_MAX_SPEED = 0.3; //auto alignment speed is clipped to minimum negative this and maximum positive this (bilateral tolerance)
     public static double AUTO_ALIGN_GAIN = 0.02; //converts degrees to power, at a 1:100 ratio (ex: 25 degrees = 0.25 power)
 
+    //TODO: tune this gain, currently overshooting a little
+
     private OpMode opMode;
 
 
