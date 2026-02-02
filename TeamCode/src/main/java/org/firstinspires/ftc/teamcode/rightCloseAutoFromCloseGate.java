d39c9f9f8bbe399026bf21306c73317f2057fdf7
kw <129519318+kkkwwww-ui@users.noreply.github.com>
2026-02-02 15:09:39 -0500

    tweaked right side auto for the real field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rightCloseAutoFromCloseGate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rightCloseAutoFromCloseGate.java
@@ -36,7 +36,7 @@ public class rightCloseAutoFromCloseGate extends LinearOpMode {
     public static double AUTO_Y_OFFSET = 0;
     public static double INTAKE_X_OFFSET = 0;
 //    public static double RELEASE_BALLS_Y = 74.2;
-    public static double COLLECT_BALLS_Y = 60.5;
+    public static double COLLECT_BALLS_Y = 61.5;
     public static double COLLECT_HEADING = 40.5;
     public static double SCORE_HEADING_OFFSET = -5; //score heading offset since center of goals are not exactly 45 degrees
     public static double SCORE_HEADING_PRELOAD_TOLERANCE = 0.1;
